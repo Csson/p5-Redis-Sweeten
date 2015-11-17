48e657f87144307181db999d72f3a246353961e3
Erik Carlsson <info@code301.com>
2015-11-17 11:39:38 +0000

    Init

---
+++ lib/Redis/Sweeten.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package Redis::Sweeten;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Redis::Sweeten;
+
+=head1 DESCRIPTION
+
+Redis::Sweeten is ...
+
+=head1 SEE ALSO
+
+=cut
